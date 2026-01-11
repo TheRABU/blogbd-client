8ae3f600f9118585e9463f729a33a9438e839363
TheRABU <mdfazlerabbi114@gmail.com>
2026-01-11 15:19:26 +0600

    fixed the middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,17 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
 
-export const config = { matcher: ["/dashboard"] };
+export default withAuth({
+  callbacks: {
+    authorized: ({ token }) => {
+      return !!token;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
+});
+
+// Protect these routes
+export const config = {
+  matcher: ["/dashboard/:path*", "/my-blogs/:path*"],
+};
